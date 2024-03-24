29ff5af9a142fa2a05224b408753433dcb71db53
SurajBargaje <surajbargaje.scoe.mechanical@gmail.com>
2024-03-24 12:34:06 +0530

    list practice

---
+++ CollectionPractice/List/MyList.java
@@ -0,0 +1,27 @@
+package List;
+import java.util.*;
+// import java.util.ArrayList;
+
+public class MyList {
+    
+    public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
+        
+
+        myList.add("Suraj");
+        myList.add("Savita");
+        myList.add("abhishek");
+
+        myList.add("himanshu");
+
+        System.out.println(myList);
+
+        System.out.println(myList.isEmpty());
+        
+
+        System.out.println(myList);
+        
+    }
+   
+}
